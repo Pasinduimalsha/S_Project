f59e7d97d7351b7330438f8f9910b91092d21380
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2024-10-18 15:01:32 +0530

    python file

---
+++ animal.py
@@ -0,0 +1,27 @@
+import sys 
+
+def default():
+    print("Hello")
+
+if __name__ == '__main__':
+
+	default();
+:
+
+	default();
+:
+
+	default();
+:
+
+	default();
+:
+
+	default();
+:
+
+	default();
+:
+
+	default();
+

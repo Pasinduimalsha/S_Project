bc48b3aceddd71c7256d141408ac4afacb0011b3
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2024-10-18 15:39:09 +0530

    c1

--- animal.py
+++ animal.py
@@ -1,27 +1,11 @@
 import sys 
-
+def dog():
+    print("Subii")
 def default():
     print("Hello")
 
 if __name__ == '__main__':
 
 	default();
-:
 
-	default();
-:
-
-	default();
-:
-
-	default();
-:
-
-	default();
-:
-
-	default();
-:
-
-	default();
 
